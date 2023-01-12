379576129de6fb192e7a4a537bfc274476259221
Dom Binks <dominic.binks@gmail.com>
2023-01-12 00:10:31 +0000

    Add warmup solutions

---
+++ warmup/Birthday-Cake-Candles.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    heights = {}
+    for candle in candles:
+        if candle in heights.keys():
+            heights[candle] += 1
+        else:
+            heights[candle] = 1
+    
+    return max(heights.values())
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
---
+++ warmup/Diagonal-Difference.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    primary = 0
+    secondary = 0
+    
+    counter = 0
+    for row in arr:
+        primary += row[0 + counter]
+        secondary += row[-1 - counter]
+        counter += 1
+        
+    return abs(primary - secondary)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
